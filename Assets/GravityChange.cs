d5b4c32e3011f2846fe4d0fc0fd7239fb5a5e7bb
Marc Dubois <marc@fragment.in>
2015-05-26 10:48:47 +0200

    add a lot

---
+++ Assets/GravityChange.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityChange : MonoBehaviour {
+	float time = 0;
+	Vector3 wind;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time ++;
+		wind.x = Mathf.Sin(time/30f)*5;
+		wind.z = -9.81f;
+		wind.y = Mathf.Cos(time/30f)*5;
+		Physics.gravity = wind;
+	}
+}
---
+++ Assets/OnOffCloth.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnOffCloth : MonoBehaviour {
+	Cloth cloth;
+	// Use this for initialization
+	void Start () {
+		cloth = gameObject.GetComponent<Cloth>() as Cloth;
+		cloth.enabled = false;
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnBecameVisible () {
+		cloth.enabled = true;
+	}
+	void OnBecameInvisible () {
+		cloth.enabled = false;
+	}
+}
