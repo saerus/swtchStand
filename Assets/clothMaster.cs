817261fe67aefd7679d8c76859739dadf62172f6
Marc Dubois <marc@fragment.in>
2015-06-16 00:41:12 +0200

    fine tuning

--- Assets/clothMaster.cs
+++ Assets/clothMaster.cs
@@ -12,16 +12,20 @@ public class clothMaster : MonoBehaviour {
 	float invertedFloat = 1;
 	float sizeReal;
 	public Material mat;
-	Vector2 imgSize = new Vector2();
-	Texture tempwww;
 	Vector2 offset = new Vector2();
-	//
+	public string name;
+	public Vector2 frontPos, backPos;
+	public float baseScale;
+	// -------------------------------------------------- CLOTH
+	Cloth cloth;
+	public GameObject clothGO;
+	GameObject main;
 	//
 	void Start () {
-		StartCoroutine(LoadFromURL());
-
+		cloth = clothGO.GetComponent<Cloth>() as Cloth;
+		cloth.enabled = false;
+		main = GameObject.Find ("_MAIN");
 	}
-	
 	// Update is called once per frame
 	void Update () {
 		if (inverted) {
@@ -29,29 +33,64 @@ public class clothMaster : MonoBehaviour {
 		} else {
 			invertedFloat = 1;
 		}
-		if(tempwww) {
+		//if(tempwww) {
 			// de plein à un diviseur de l'image (10 / 1/10)
+			mat = clothGO.gameObject.GetComponent<Renderer> ().materials [1];
 			mat.SetTextureScale("_MainTex", new Vector2(size, size*invertedFloat));
 			sizeReal = 1/size;
 			offset = new Vector2(-size/2+0.5f+(-X)*size, -size/2+0.5f+Y*size);
 			//offset.x *= invertedFloat;
 			mat.SetTextureOffset("_MainTex", offset);
+		//}
+	}
+	void setPosition(string position) {
+		if (position.Equals ("Front")) {
+			X = frontPos.x;
+			Y = frontPos.y;
+			size = baseScale;
+			inverted = false;
+		} else if (position.Equals ("Back")) {
+			X = backPos.x;
+			Y = backPos.y;
+			size = baseScale;
+			inverted = true;
+		}
+		/*X = _x;
+		Y = _y;
+		size = _size;
+		inverted = _inverted;*/
+	}
+	void updatePosition(string dir) {
+		if (dir.Equals ("Left")) {
+			X -= 0.005f;
+		} else if (dir.Equals ("Right")) {
+			X += 0.005f;
+		} else if (dir.Equals ("Up")) {
+			Y -= 0.005f;
+		} else if (dir.Equals ("Down")) {
+			Y += 0.005f;
+		} else if (dir.Equals ("Bigger")) {
+			size -= 2f;
+		} else if (dir.Equals ("Smaller")) {
+			size += 2f;
 		}
 	}
-	IEnumerator LoadFromURL() {
-		// Start a download of the given URL
-		WWW www = new WWW("http://www.saerus.ch/logoonlinetest.png");
-		
-		// Wait for download to complete
-		yield return www;
-		tempwww = www.texture;
-		imgSize.x = tempwww.width;
-		imgSize.y = tempwww.height;
-		Debug.Log (imgSize.x);
-		
-		// assign texture
-		tempwww.wrapMode = TextureWrapMode.Clamp;
-		Renderer renderer = GetComponent<Renderer>();
-		mat.mainTexture = tempwww;
+	void changeColor(Color c) {
+		//Debug.Log ("**********");
+		clothGO.gameObject.GetComponent<Renderer>().materials[0].color = c;
+		clothGO.gameObject.GetComponent<Renderer> ().materials [2].SetColor("_Emission", c);
+	}
+	void OnBecameVisible () {
+		cloth.enabled = true;
+		main.BroadcastMessage("setCloth", name, SendMessageOptions.DontRequireReceiver);
+
+	}
+	void changeImg(Texture2D newImg) {
+		Debug.Log (clothGO.gameObject.GetComponent<Renderer> ().materials [1].name);
+		clothGO.gameObject.GetComponent<Renderer> ().materials [1].SetTexture("_MainTex", newImg);
+
+	}
+	void OnBecameInvisible () {
+		cloth.enabled = false;
 	}
 }
--- Assets/script/SendSocketIO.cs
+++ Assets/script/SendSocketIO.cs
@@ -1,35 +1,8 @@
-﻿#region License
-/*
- * TestSocketIO.cs
- *
- * The MIT License
- *
- * Copyright (c) 2014 Fabio Panettieri
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-#endregion
-
-using System.Collections;
+﻿using System.Collections;
 using UnityEngine;
 using SocketIO;
-using System;
+using System.IO;
+//using System;
 
 public class SendSocketIO : MonoBehaviour
 {
@@ -41,14 +14,17 @@ public class SendSocketIO : MonoBehaviour
 
 	//*IMPORTANT* A remplacer par le bon objet ! 
 	public GameObject textureHolder;
+	Texture2D textureKeeper;
 
+	GameObject currentCloth;
+	public GameObject[] cloths;
 	//Coroutine pour charger un logo
 	IEnumerator LoadImg;
-	
+	//
 	public void Start() 
 	{
-		GameObject go = GameObject.Find("SocketIO");
-		socket = go.GetComponent<SocketIOComponent>();
+		//GameObject go = GameObject.Find("SocketIO");
+		socket = GetComponent<SocketIOComponent>();
 		
 		socket.On("open", onOpen);
 		socket.On("connect", onConnect);
@@ -57,27 +33,20 @@ public class SendSocketIO : MonoBehaviour
 		socket.On("error", onError);
 		socket.On("close", onClose);
 		
-		StartCoroutine("BeepBoop");
+		//StartCoroutine("BeepBoop");
 		test.AddField("cloth", "London");
 		Debug.Log ("test "+ test);
 	}
-	
-	private IEnumerator BeepBoop()
+
+	void setCloth(string cloth) {
+		JSONObject message = new JSONObject();
+		message.AddField("client","webinterface");
+		message.AddField("action","changeCloth");
+		message.AddField("cloth", cloth);
+		socket.Emit("message", message);
+	}
+	/*private IEnumerator BeepBoop()
 	{
-		// wait 1 seconds and continue
-//		yield return new WaitForSeconds(1);
-//		
-//		socket.Emit("beep", test);
-//		
-//		// wait 3 seconds and continue
-//		yield return new WaitForSeconds(3);
-//		
-//		socket.Emit("beep", test);
-//		
-//		// wait 2 seconds and continue
-//		yield return new WaitForSeconds(2);
-//		
-//		socket.Emit("beep", test);
 		yield return new WaitForSeconds(1);
 
 
@@ -101,7 +70,7 @@ public class SendSocketIO : MonoBehaviour
 //		
 //		socket.Emit("beep", test);
 //		socket.Emit("beep", test);
-	}
+	}*/
 
 	public void onConnect(SocketIOEvent e)
 	{
@@ -136,7 +105,7 @@ public class SendSocketIO : MonoBehaviour
 
 				//string url = "http://www.fragment.in/unity/img/cff.png";
 				//*IMPORTANT* A remplacer par le bon url !
-				string baseUrl = "localhost:8888/switcher/server/";
+				string baseUrl = "localhost:8888/";
 				string imgUrl = e.data.GetField("url").str;
 
 				WWW www = new WWW(baseUrl+imgUrl);
@@ -145,22 +114,52 @@ public class SendSocketIO : MonoBehaviour
 
 				LoadImg = DownloadImage(www);
 				StartCoroutine(LoadImg);
-			}else if(action.Equals("changeColor")){
+			} else if (action.Equals("changeColor")){
 
 				//*IMPORTANT* A remplacer par le bon objet !
-				color = ParseColor(e.data.GetField("color").str); 
-				//Debug.Log(color);
+				color = ParseColor(e.data.GetField("color").str);
+				foreach (GameObject go in cloths) {
+					go.BroadcastMessage("changeColor", color, SendMessageOptions.DontRequireReceiver);
+				}
+				//Debug.Log("***********- "+color);
 
 			/*Set position first once */
-			}else if(action.Equals("setPosition")){
+			} else if(action.Equals("changeCloth")) {
 
+				string position = e.data.GetField("name").str;
+				Debug.Log("%%% CHANGE CLOTHHRTHH  "+position);
+				foreach (GameObject go in cloths) {
+					go.SetActive(false);
+				}
+				if(position.Equals("Bob")) {
+					cloths[0].SetActive(true);
+				} else if (position.Equals("Steve")) {
+					cloths[1].SetActive(true);
+
+				} else if(position.Equals("Victor")) {
+					cloths[2].SetActive(true);
+
+				} else if(position.Equals("John")) {
+					cloths[3].SetActive(true);
+					
+				} else if(position.Equals("London")) {
+					cloths[4].SetActive(true);
+					
+				}
+			} else if(action.Equals("setPosition")){
+
+
+				string position = e.data.GetField("position").str;
+				foreach (GameObject go in cloths) {
+					go.BroadcastMessage("setPosition", position, SendMessageOptions.DontRequireReceiver);
+				}
 				//*IMPORTANT* A remplacer par le bon objet !
-				float x = float.Parse(e.data.GetField("x").str);
+				/*float x = float.Parse(e.data.GetField("x").str);
 				float y = float.Parse(e.data.GetField("y").str);
 				float size = float.Parse(e.data.GetField("size").str);
 				bool inverted = bool.Parse(e.data.GetField("inverted").str);
 
-				Debug.Log(" x : "+x+" / y : "+y+" / size : "+size+" / inverted : "+inverted);
+				Debug.Log(" x : "+x+" / y : "+y+" / size : "+size+" / inverted : "+inverted);*/
 			}
 			/*Update position after */
 			else if(action.Equals("updatePosition")){
@@ -168,12 +167,16 @@ public class SendSocketIO : MonoBehaviour
 				//*IMPORTANT* A remplacer par le bon objet !
 				string position = e.data.GetField("position").str; 
 				Debug.Log("position : "+position);
+				// SEND
+				foreach (GameObject go in cloths) {
+					go.BroadcastMessage("updatePosition", position, SendMessageOptions.DontRequireReceiver);
+				}
 			}
 			/*Take screenshot*/
-			else if(action.Equals("takeScreenshot")){
+			else if(action.Equals("takeSnapshot")){
 				
 				//*IMPORTANT* A remplacer par le bon objet !
-				Debug.Log("take screenshot ");
+				Debug.Log("take screenshot YEAH");
 
 				//Try and catch doesn't work properly
 //				try{
@@ -191,36 +194,116 @@ public class SendSocketIO : MonoBehaviour
 //				Debug.Log("screenshot taken ");
 
 				//save to persistent data path otherwise unity throw an error / because it is IOS ?
-				string persistentDataPath = null;
+				string uniqueName = System.DateTime.Now.ToString("yyyyMMdd_HHmmss");
+				Debug.Log(uniqueName);
+				string imgPath = "contacts/"+uniqueName+".png";
+				//Debug.Log("$$$ "+Application.dataPath);
+				Application.CaptureScreenshot(imgPath);
+				//
+				//
+				string fileName = "contacts/contacts.txt";
+				//File.WriteAllLines();
+				string entreprise = e.data.GetField("entreprise").str; 
+				string nom = e.data.GetField("nom").str; 
+				string prenom = e.data.GetField("prenom").str; 
+				string email = e.data.GetField("email").str;
+
+				File.AppendAllText(fileName, uniqueName+"*#*"+entreprise+"*#*"+nom+"*#*"+prenom+"*#*"+email+"\n");
+
+				//Debug.Log("#########: "+Application.dataPath);
+				// SEND TO SERVER AND MAIL !!
+
+
+				StartCoroutine(UploadPNG(nom, prenom, entreprise, email, uniqueName));
+				/*string persistentDataPath = null;
 				if (persistentDataPath == null)
 					persistentDataPath = Application.persistentDataPath;        
 				Debug.Log("Data Path =  " + persistentDataPath); // If you want to easily see where that is.
 				Application.CaptureScreenshot(persistentDataPath+"Screenshot.png");
-				Debug.Log("screenshot taken ");
+				Debug.Log("screenshot taken ");*/
 
+			} else if(action.Equals("changeCloth")){
+				
+				//*IMPORTANT* A remplacer par le bon objet !
+				string name = e.data.GetField("name").str; 
+				Debug.Log("name : "+name);
+
+				if(currentCloth) {
+					currentCloth.SetActive(false);
+				}
+
+				//currentCloth = GameObject.Find(name+"Target") as GameObject;
+				int r = Random.Range(0, cloths.Length);
+				currentCloth = cloths[r];
+				Debug.Log("***CURRENT: "+currentCloth);
+				currentCloth.SetActive(true);
+				//GameObject cloth = 
 			}
 		}
 	}
-
+	IEnumerator UploadPNG(string nom, string prenom, string entreprise, string email, string uniqueName) {
+		// We should only read the screen after all rendering is complete
+		yield return new WaitForEndOfFrame();
+		
+		// Create a texture the size of the screen, RGB24 format
+		int width = Screen.width;
+		int height = Screen.height;
+		var tex = new Texture2D(width, height, TextureFormat.RGB24, false );
+		
+		// Read screen contents into the texture
+		tex.ReadPixels( new Rect(0, 0, width, height), 0, 0 );
+		tex.Apply();
+		
+		// Encode texture into PNG
+		byte[] bytes = tex.EncodeToPNG();
+		Destroy( tex );
+		
+		// Create a Web Form
+		WWWForm form = new WWWForm();
+		//form.AddField("frameCount", Time.frameCount.ToString());
+		form.AddField ("nom", nom);
+		form.AddField ("prenom", prenom);
+		form.AddField ("entreprise", entreprise);
+		form.AddField ("email", email);
+		form.AddField ("uniqueName", uniqueName);
+		form.AddBinaryData("image", bytes, "screenShot.png", "image/png");
+		
+		// Upload to a cgi script
+		WWW w = new WWW("http://www.fragment.in/wp/switcher/mail.php", form);
+		yield return w;
+		if (!string.IsNullOrEmpty(w.error)) {
+			print(w.error);
+		}
+		else {
+			print("Finished Uploading Screenshot");
+			Debug.Log("!!!!! "+w.text);
+		}
+	}
 	IEnumerator DownloadImage(WWW wwwimg2){
 
 		Debug.Log ("Started DownloadImage ");
 		
 		// Create a texture in DXT1 format
-		textureHolder.GetComponent<Renderer>().material.mainTexture = new Texture2D(4, 4, TextureFormat.DXT1, false);
-		
-		while(true) {
 
-			// wait until the download is done
-			yield return wwwimg2;
+		//textureHolder.GetComponent<Renderer>().material.mainTexture = new Texture2D(4, 4, TextureFormat.DXT1, false);
+		textureKeeper = new Texture2D(4, 4, TextureFormat.DXT1, false);
+
+		// wait until the download is done
+		yield return wwwimg2;
 			
 			// assign the downloaded image to the main texture of the object
-			wwwimg2.LoadImageIntoTexture(textureHolder.GetComponent<Renderer>().material.mainTexture as Texture2D);
+		wwwimg2.LoadImageIntoTexture(textureKeeper);
+		textureKeeper.wrapMode = TextureWrapMode.Clamp;
+		textureKeeper.alphaIsTransparency = true;
 
+
+			foreach (GameObject go in cloths) {
+				go.BroadcastMessage("changeImg", textureKeeper, SendMessageOptions.DontRequireReceiver);
+			}
 			Debug.Log ("Image replaced");
 			//yield return null;
 			StopCoroutine(LoadImg);
-		}
+
 		
 	}
 	
@@ -264,7 +347,6 @@ public class SendSocketIO : MonoBehaviour
 	{	
 		Debug.Log("[SocketIO] Close received: " + e.name + " " + e.data);
 	}
-
 
 	
 }
